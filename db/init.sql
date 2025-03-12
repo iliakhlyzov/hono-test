25e1553107d98dfac43302b85ded119f63d15f72
iliakhlyzov-verkstedt <146713254+iliakhlyzov-verkstedt@users.noreply.github.com>
2025-03-12 12:27:41 +0300

    fix: improve init sql

--- db/init.sql
+++ db/init.sql
@@ -1,20 +1,20 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE users (
-                       id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-                       name TEXT NOT NULL,
-                       balance DECIMAL(10,2) NOT NULL DEFAULT 0.00 CHECK (balance >= 0)
-);
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    balance DECIMAL(10,2) NOT NULL DEFAULT 0.00 CHECK (balance >= 0)
+    );
 
-CREATE TABLE products (
-                          id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-                          name TEXT NOT NULL,
-                          price DECIMAL(10,2) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS products (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    price DECIMAL(10,2) NOT NULL CHECK (price >= 0)
+    );
 
-CREATE TABLE purchases (
-                           id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-                           user_id UUID REFERENCES users(id) ON DELETE CASCADE,
-                           product_id UUID REFERENCES products(id) ON DELETE CASCADE,
-                           purchase_date TIMESTAMP DEFAULT NOW()
-);
+CREATE TABLE IF NOT EXISTS purchases (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    product_id UUID REFERENCES products(id) ON DELETE CASCADE,
+    purchase_date TIMESTAMP DEFAULT NOW()
+    );
