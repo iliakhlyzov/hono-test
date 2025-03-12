50aab100e4097c43be5d0deb453cc157c3902cd0
iliakhlyzov-verkstedt <146713254+iliakhlyzov-verkstedt@users.noreply.github.com>
2025-03-12 12:24:51 +0300

    feat: check balance

--- db/init.sql
+++ db/init.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE users (
                        id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
                        name TEXT NOT NULL,
-                       balance DECIMAL(10,2) NOT NULL DEFAULT 0.00
+                       balance DECIMAL(10,2) NOT NULL DEFAULT 0.00 CHECK (balance >= 0)
 );
 
 CREATE TABLE products (
