85e6fef430197e5028dd55e7ced8cc23ba8ba919
iliakhlyzov-verkstedt <146713254+iliakhlyzov-verkstedt@users.noreply.github.com>
2025-03-12 11:05:47 +0300

    feat: add ioredis (cacheService), docker-compose, dockerfile, makefile, skinportService

---
+++ src/externalApi/skinportService.ts
@@ -0,0 +1,23 @@
+import {SkinportGetItemsResponse} from "../types/externalApi/skinportService";
+
+class SkinportService {
+    async getItemsV1 (appId: number = 730, currency: string = 'EUR', tradable: number = 0): Promise<SkinportGetItemsResponse> {
+
+    const params = new URLSearchParams({
+        app_id: String(appId),
+        currency,
+        tradable: String(tradable),
+    })
+
+    const response = await fetch(`https://api.skinport.com/v1/items?${params}`, {
+        method: 'GET',
+        headers: {
+            'Accept-Encoding': 'br'
+        }
+    });
+
+    return await response.json();
+    }
+}
+
+export const skinportService = new SkinportService()
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
 import { Hono } from 'hono'
+import { etag } from 'hono/etag'
+import { logger } from 'hono/logger'
+import {cacheService} from "./services/cacheService";
+import {SkinportGetItemsResponse} from "./types/externalApi/skinportService";
+import {skinportService} from "./externalApi/skinportService";
 
 const app = new Hono()
 
+app.use(etag(), logger())
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.get('/skinport', async (c) => {
+  const cacheKey = "skinport:data";
+  const ttl = 300
+
+  const data = await cacheService.getOrSet<SkinportGetItemsResponse>(cacheKey, async () => {
+    return skinportService.getItemsV1()
+  }, ttl);
+
+  return c.json(data);
 })
 
 export default app
